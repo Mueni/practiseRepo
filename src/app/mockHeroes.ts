532f00531184a443ee6169980bd09bfcb649fce6
Mueni <4missmusau@gmail.com>
2020-08-10 22:37:41 +0300

    Display list

---
+++ src/app/mockHeroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] =
+[
+  {id: 21, name: 'Mwendwa'},
+  {id: 22, name: 'Mwende'},
+  {id: 23, name: 'Mwendia'},
+  {id: 24, name: 'Mueni'},
+  {id: 25, name: 'Mutuku'},
+  {id: 26, name: 'Musau'},
+  {id: 27, name: 'Musyoka'},
+  {id: 28, name: 'Musyoki'},
+  {id: 29, name: 'Mutheu'},
+  {id: 20, name: 'Muasya'},
+];
